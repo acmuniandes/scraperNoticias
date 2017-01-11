245ab300eddbb6a6dec8be5f58564c4d97a4d3d1
Ignacio Gomez <i.gomez10@uniandes.edu.co>
2017-01-11 16:15:00 -0500

    V-1

---
+++ scraperElTiempo.py
@@ -0,0 +1,42 @@
+from bs4 import BeautifulSoup
+import csv
+import html5lib
+from urllib.request import urlopen
+import schedule
+import time
+import datetime
+class articulo:
+    titulo=''
+    link=''
+    contenido=''
+listaArticulos=[]
+
+def scrape():
+    print(datetime.datetime.now())
+    soup = BeautifulSoup(urlopen("http://www.eltiempo.com"),'html5lib')
+    for unArticulo in soup.find_all('div',class_="main_article"):
+        nuevoArticulo=articulo()
+        nuevoArticulo.titulo=unArticulo.a.string
+        nuevoArticulo.link=unArticulo.a['href']
+        nuevoArticulo.contenido=""
+
+
+        if nuevoArticulo.link.startswith('/'):
+            nuevoArticulo.link= "http://www.eltiempo.com" + nuevoArticulo.link
+        noodles = BeautifulSoup(urlopen(nuevoArticulo.link),'html5lib')
+        nuevoArticulo.contenido = (noodles.find('div',id="contenido"))
+        listaArticulos.append(nuevoArticulo)
+
+    with open("output.csv", "w") as f:
+        writer = csv.writer(f)
+        writer.writerow(["titulo", "link", "contenido"])
+        for a in listaArticulos:
+            writer.writerow([a.titulo,a.link, a.contenido])
+    print(datetime.datetime.now())
+
+scrape()
+schedule.every(5).minutes.do(scrape)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
