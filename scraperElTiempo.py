e56523cb9672a9cebb8149589b3761cbd91eb52d
Ignacio Gomez <i.gomez10@uniandes.edu.co>
2017-01-15 21:23:22 -0500

    Avances

--- scraperElTiempo.py
+++ scraperElTiempo.py
@@ -7,40 +7,74 @@ import time
 import datetime
 import redis
 import os
+import requests
+
 class articulo:
     titulo=''
     link=''
     contenido=''
-
+USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36"
 def scrape():
     listaArticulos=[]
-    print(datetime.datetime.now())
-    soup = BeautifulSoup(urlopen("http://www.eltiempo.com"),'html5lib')
+    log("solicitando principal")
+    request("http://www.futbolred.com/liga-bbva/real-madrid-encadena-40-partidos-invicto+16791919")
+    page = request("http://eltiempo.com")
+
+    soup = BeautifulSoup( page , 'html5lib')
+
     for unArticulo in soup.find_all('div',class_="main_article"):
         nuevoArticulo=articulo()
         nuevoArticulo.titulo=unArticulo.a.string
         nuevoArticulo.link=unArticulo.a['href']
         nuevoArticulo.contenido=""
 
-
-        if nuevoArticulo.link.startswith('/'):
-            nuevoArticulo.link= "http://www.eltiempo.com" + nuevoArticulo.link
-        noodles = BeautifulSoup(urlopen(nuevoArticulo.link),'html5lib')
+        is_relative_article_link = nuevoArticulo.link.startswith('/')
+        if is_relative_article_link:
+            nuevoArticulo.link = "http://www.eltiempo.com" + nuevoArticulo.link
+        noodles = BeautifulSoup(request(nuevoArticulo.link),'html5lib')
         nuevoArticulo.contenido = (noodles.find('div',id="contenido"))
         listaArticulos.append(nuevoArticulo)
 
-
-
-
-
-    r = redis.from_url(os.environ.get('REDIS_URL'))
-    with open("output.csv", "w") as f:
-        writer = csv.writer(f)
-        writer.writerow(["titulo", "link", "contenido"])
-        for a in listaArticulos:
-            writer.writerow([a.titulo,a.link, a.contenido])
+    elcsv = serialize_articles(listaArticulos)
+    store(elcsv)
     print(datetime.datetime.now())
-    r.set('news' , listaArticulos)
+
+def request(url):
+    log("requesting " + url)
+    custom_headers = {'user-agent' : USER_AGENT , 'accept': "text/html;charset=UTF-8"  }
+    response = requests.get(url , headers = custom_headers)
+    response.encoding="utf-8"
+    log(response.encoding)
+    return response.text
+
+
+
+def log(algo):
+    prefix = '[' + timestamp() + '] '
+    print( prefix + algo)
+
+def timestamp():
+    return datetime.datetime.now().strftime("%m/%d/%y %H:%M:%S")
+
+
+def serialize_article(article):
+    line_elements = [
+        str(article.titulo),
+        str(article.link),
+        str(article.contenido)
+    ]
+    final_line = ','.join(line_elements)
+    return final_line
+
+def serialize_articles(lista):
+    articulos = map(serialize_article, lista)
+    return '\n'.join(articulos)
+
+def store(content):
+    r = redis.from_url(os.environ.get('REDIS_URL'))
+    r.set('news' , content.encode('utf8'))
+
+
 
 scrape()
 schedule.every(5).minutes.do(scrape)
---
+++ server.py
@@ -0,0 +1,23 @@
+import os
+import redis
+from flask import Flask , send_from_directory
+
+r = redis.from_url(os.environ.get('REDIS_URL'))
+
+app = Flask(__name__, static_url_path='')
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route("/news")
+def show():
+    return r.get('news').decode('utf8')
+
+@app.route("/<path:path>")
+def serve_file(path):
+    app.logger.debug(path)
+    return send_from_directory('public', path)
+
+if __name__ == "__main__":
+    app.run()
